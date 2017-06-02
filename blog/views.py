84b49df1c4b21bd0bf99402e7d2e1c06c634ebbc
aiterator <769671058@qq.com>
2017-06-02 11:28:22 +0800

    add IndexPage and css

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+from blog.models import *
+def Index(request):
+    articles = Article.objects.order_by('-time')
+    return render(request, 'index.html', {'title':'aiterator', 'articles': articles})
--- blog_one/urls.py
+++ blog_one/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-
+from blog.views import *
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', Index, name='index'),
 ]
